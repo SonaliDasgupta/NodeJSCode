1ca76bc4d7d2f68757c87bd9a5e71297dd80f22a
SonaliDasgupta <sonalidasgupta95.2011@gmail.com>
2018-07-31 22:00:22 +0530

    Using inbuilt Node modules

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 const http= require('http');
+const fs= require('fs');
+const path=require('path');
 
 const hostname ='localhost';
 const port = 3000;
 
 const server=http.createServer((req, res) => {
-	console.log(req.headers);
-	res.statusCode = 200;
-	res.setHeader('Content-Type','text/html');
-	res.end('<html><body><h1>Hello World!</h1></body></html>' );
+	console.log("Request for "+req.url+" by method "+req.method);
+	if(req.method == 'GET'){
+		var fileUrl;
+		if(req.url=='/') {
+			fileUrl= '/index.html';
+		}
+		else
+			fileUrl= req.url;
+		console.log(fileUrl);
+		var filePath= path.resolve('./public'+fileUrl);
+		console.log(filePath);
+		const file_extension = path.extname(filePath);
+		if(file_extension == '.html'){
+			fs.exists(filePath, (exists) => {
+				if(!exists){
+					res.statusCode=404;
+					res.setHeader('Content-Type','text/html');
+					res.end('<html><body><h1>Error 404: '+fileUrl+' not found.</h1></body></html>');
+					return ;
+				}
+				
+				res.statusCode=200;
+				res.setHeader('Content-Type','text/html');
+				fs.createReadStream(filePath).pipe(res);
+				
+			}
+			
+			)
+		}
+		else{
+			res.statusCode=404;
+					res.setHeader('Content-Type','text/html');
+					res.end('<html><body><h1>Error 404: '+fileUrl+' not an HTML file.</h1></body></html>');
+					return ;
+		}
+			
+	}
+	else{
+		res.statusCode=405;
+		res.setHeader('Content-Type','text/html');
+		res.end('<html><body><h1>Error 405: '+req.method+' not allowed.</h1></body></html>');
+		return ;
+	}
 })
 	
 	server.listen(port, hostname, ()=> {
